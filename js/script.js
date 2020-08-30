4846ece35a9d2bd7d260ebd6a8adce0bf3178ecc
Andrew Leach <andrew_leach@outlook.com>
2020-08-29 23:35:41 -0500

    Added final stylized UI

--- js/script.js
+++ js/script.js
@@ -1,25 +1,117 @@
 
     let player = prompt("What's your name?"); // Asks the player to enter name before page loads
+    
+    const playerName = document.querySelector('#player-score');
+    playerName.textContent = player;
+
     let playerCounter = 0;
     let computerCounter = 0;
 
-    const playerScore = document.querySelector('#player-score');
-    playerScore.textContent = player + "'s Score: " + playerCounter;
+    const playerScore = document.createElement('div');
+    playerScore.textContent = playerCounter;
+    playerScore.className = "player-counter";
+    playerName.appendChild(playerScore);
 
-    const computerScore = document.querySelector('#computer-score');
-    computerScore.textContent = "Computer's Score: " + computerCounter;
+    const computerScore = document.querySelector('.computer-counter');
+    computerScore.textContent = computerCounter;
+
+    // Animations 
+    const animationPlayer = document.querySelector('animation-container');
+
+    const lostPapertoScissors = lottie.loadAnimation( {
+        container: document.getElementById('animation-container'),
+        path: 'animations/lost-paper-to-scissors/lost-paper-to-scissors.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "lostPapertoScissors"
+    })
+
+    const lostScissorstoRock = lottie.loadAnimation( {
+        container: document.getElementById('animation-container'),
+        path: 'animations/lost-scissors-to-rock/lost-scissors-to-rock.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "lostScissorstoRock"
+    })
+
+    const lostRocktoPaper = lottie.loadAnimation( {
+        container: document.getElementById('animation-container'),
+        path: 'animations/lost-rock-to-paper/lost-rock-to-paper.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "lostRocktoPaper"
+    })
+
+    const tiePaper = lottie.loadAnimation( {
+        container: document.getElementById('animation-container'),
+        path: 'animations/tie-paper/tie-paper.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "tiePaper"
+    })
+
+    const tieRock = lottie.loadAnimation( {
+        container: document.getElementById('animation-container'),
+        path: 'animations/tie-rock/tie-rock.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "tieRock"
+    })
+
+    const tieScissors = lottie.loadAnimation( {
+        container: document.getElementById('animation-container'),
+        path: 'animations/tie-scissors/tie-scissors.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "tieScissors"
+    })
+
+    const wonPaperBeatsRock = lottie.loadAnimation( {
+        container: document.getElementById('animation-container'),
+        path: 'animations/won-paper-beats-rock/won-paper-beats-rock.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "wonPaperBeatsRock"
+    })
+
+    const wonRockBeatsScissors = lottie.loadAnimation( {
+        container: document.getElementById('animation-container'),
+        path: 'animations/won-rock-beats-scissors/won-rock-beats-scissors.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "wonRockBeatsScissors"
+    })
+
+    const wonScissorsBeatsPaper = lottie.loadAnimation( {
+        container: document.getElementById('animation-container'),
+        path: 'animations/won-scissors-beats-paper/won-scissors-beats-paper.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "wonScissorsBeatsPaper"
+    })
+
+
 
     const messageBanner = document.querySelector('#message-banner');
-    messageBanner.textContent = "Ready to play? First to five wins - shoot!";
-
+    messageBanner.textContent = "Ready to play? Shoot!";
    
     // set scores back to zero, reset the text and update the message banner
     function resetRound() {
         playerCounter = 0;
         computerCounter = 0;
-        computerScore.textContent = "Computer's Score: " + computerCounter;
-        playerScore.textContent = player + "'s Score: " + playerCounter;
+        computerScore.textContent = computerCounter;
+        playerScore.textContent = playerCounter;
         messageBanner.textContent = "Ready to play again? Shoot!";
+        lottie.stop();
 
     }
 
@@ -54,16 +146,16 @@
                     // Play a single round - Have user enter their choice, count scores, roll computer's turn, and display a message
     function playRound(userSelection, computerSelection) {
 
-        // Display victory or defeat message to user and add to the total scores - this if/else-if statement is written as a tenerary operator for shorthand                return  userSelection == "rock" && computerSelection == "rock" ? console.log("Lame. Guess y'all just like rocks then") :
-        userSelection == "rock" && computerSelection == "paper" ? (messageBanner.textContent = "Sorry pal, paper beats rock :(", ++computerCounter, computerScore.textContent = "Computer's Score: " + computerCounter) :
-        userSelection == "rock" && computerSelection == "scissors" ? (messageBanner.textContent = "Hell yeah, you sure crushed those scissors!", ++playerCounter, playerScore.textContent = player + "'s Score: " + playerCounter) :
-        userSelection == "rock" && computerSelection == "rock" ? messageBanner.textContent = " Tie - Lame, guess y'all just like rocks" :
-        userSelection == "paper" && computerSelection == "rock" ? (messageBanner.textContent = "Hell yeah, you sure beat that rock", ++playerCounter, playerScore.textContent = player + "'s Score: " + playerCounter) :
-        userSelection == "paper" && computerSelection == "paper" ? messageBanner.textContent = "Tie - Lame. Guess y'all just like paper then" :
-        userSelection == "paper" && computerSelection == "scissors" ? (messageBanner.textContent = "Sorry pal, you just got beat by some scissors :(", ++computerCounter, computerScore.textContent = "Computer's Score: " + computerCounter) :
-        userSelection == "scissors" && computerSelection == "rock" ? (messageBanner.textContent = "Sorry pal, you got beat by a rock :(", ++computerCounter, computerScore.textContent = "Computer's Score: " + computerCounter) :
-        userSelection == "scissors" && computerSelection == "paper" ? (messageBanner.textContent = "Hell yeah, you sure cut that paper", ++playerCounter, playerScore.textContent = player + "'s Score: " + playerCounter) : 
-        userSelection == "scissors" && computerSelection == "scissors" ? messageBanner.textContent = "Tie - Lame. Guess y'all just really like scissors"
+        // Display victory or defeat message to user and add to the total scores - this if/else-if statement is written as a tenerary operator for shorthand 
+        userSelection == "rock" && computerSelection == "paper" ? (messageBanner.textContent = "Sorry pal, paper beats rock :(", ++computerCounter, computerScore.textContent = computerCounter, lottie.stop(), lottie.play("lostRocktoPaper")) :
+        userSelection == "rock" && computerSelection == "scissors" ? (messageBanner.textContent = "Hell yeah, you sure crushed those scissors!", ++playerCounter, playerScore.textContent = playerCounter, lottie.stop(), lottie.play("wonRockBeatsScissors")) :
+        userSelection == "rock" && computerSelection == "rock" ? (messageBanner.textContent = "Tie - Lame, guess y'all just like rocks", lottie.stop(), lottie.play("tieRock")) :
+        userSelection == "paper" && computerSelection == "rock" ? (messageBanner.textContent = "Hell yeah, you sure beat that rock", ++playerCounter, playerScore.textContent = playerCounter, lottie.stop(), lottie.play("wonPaperBeatsRock")) :
+        userSelection == "paper" && computerSelection == "paper" ? (messageBanner.textContent = "Tie - Lame. Guess y'all just like paper then", lottie.stop(), lottie.play("tiePaper")) :
+        userSelection == "paper" && computerSelection == "scissors" ? (messageBanner.textContent = "Sorry pal, you got beat by some scissors :(", ++computerCounter, computerScore.textContent = computerCounter, lottie.stop(), lottie.play("lostPapertoScissors")) :
+        userSelection == "scissors" && computerSelection == "rock" ? (messageBanner.textContent = "Sorry pal, you got beat by a rock :(", ++computerCounter, computerScore.textContent = computerCounter, lottie.stop(), lottie.play("lostScissorstoRock")) :
+        userSelection == "scissors" && computerSelection == "paper" ? (messageBanner.textContent = "Hell yeah, you sure cut that paper", ++playerCounter, playerScore.textContent = playerCounter, lottie.stop(), lottie.play("wonScissorsBeatsPaper")) : 
+        userSelection == "scissors" && computerSelection == "scissors" ? (messageBanner.textContent = "Tie - Lame. Guess y'all just really like scissors", lottie.stop(), lottie.play("tieScissors"))
             : "You forgotten how to spell? Try again";
 
         
--- readme.md
+++ readme.md
@@ -7,11 +7,14 @@ https://andrewleach94.github.io/Rock-Paper-Scissors/
 
 
 #### Overview
-Play against the computer with a simple game of rock, paper, scissors. It's been most recently updated to include a basic UI - removing the need to open the console to play. This project will be updated to improve the overall UX by adding a great deal of CSS to overhaul the visual design.
+Play against the computer with a simple game of rock, paper, scissors. The most recent update added a totally revamped UI with animations and working links. The next update will improve the overall UX by replacing the harsh alerts and prompts alongside some other tweaks. 
+
+This is currently best experienced on a mobile device.
 
 
 ##### Checklist
 
 - [x] Develop the core backend component of the game - (8/18/2020)
 - [x] Develop a basic user interface to remove need of the console (8/25/2020)
-- [ ] Design and develop the final UI
+- [x] Design and develop the final UI (8/29/2020)
+- [ ] Improve the UX for alerts, prompts, and create custom styles for desktop viewing. 
